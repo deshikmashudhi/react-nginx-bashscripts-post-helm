fe6715e18bb21e1f4a4a3fa52ba8c7a86f632009
Hari <118122494+Hari0o@users.noreply.github.com>
2024-07-26 21:37:58 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 cd /home/ubuntu/react-chart/
 git pull
+aws s3 cp s3://gitcommitids/old_value.txt .
+aws s3 cp s3://gitcommitids/new_value.txt .
+old_value=$(cat old_value.txt)
+new_value=$(cat new_value.txt)
+sed -i "s/${old_value}/${new_value}/g" values.yaml
 helm upgrade react-chart-helm .
+aws s3 rm s3://gitcommitids/old_value.txt
+echo $new_value > old_value.txt
+aws s3 cp old_value.txt s3://gitcommitids/
+rm old_value.txt new_value.txt
+
+
