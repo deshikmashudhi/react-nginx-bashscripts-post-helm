923947bbad3b3923b0452681fd6fc748e717bbf1
Hari <118122494+Hari0o@users.noreply.github.com>
2024-07-26 19:39:50 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 
 #!/bin/bash
-sudo docker rmi -f $(sudo docker images -q) ##this is not recommned step, i am deleting existing images to save space
+sudo docker container prune -f && sudo docker image prune -a -f && sudo docker volume prune -f && sudo docker network prune -f && sudo docker system prune -a -f
+##this above step is not recommned step, i am deleting existing images to save space
 sudo rm -r gold ## these steps are not recommened instead you can modify script as shown below
 sudo mkdir gold
 cd gold/
